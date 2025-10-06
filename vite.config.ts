9203a74822f2d53ff341808b59315af67543da3c
Fayzaya <fayzayaganangputra20@gmail.com>
2025-10-06 18:44:48 +0700

    Update package.json & vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Ganti 'repo-name' dengan nama repository GitHub-mu
 export default defineConfig({
+  base: '/lajutuju/', // <- ini penting untuk GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    port: 5173, // optional, default Vite
   },
 });
